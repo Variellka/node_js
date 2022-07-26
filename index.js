612deef12c336004330b6157a2c7e27d10bcef40
Xenia Anichkina <xenia.anichkina@oracle.com>
2022-07-26 17:45:01 +0400

    task 3 - create basic server

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const port = 3000;
+
+const products = [
+  {
+    displayName: "Cyberpank 2077",
+    price: "60$",
+  },
+  {
+    displayName: "SpongeBob SquarePants: Battle for Bikini Bottom – Rehydrated",
+    price: "40$",
+  },
+  {
+    displayName: "God Of War",
+    price: "50$",
+  },
+];
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-type": "application/json",
+  });
+  if (req.url === "/products" && req.method === "GET") {
+    return res.end(JSON.stringify(products));
+  }
+  if (req.url === "/products" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+      products.push(JSON.parse(body));
+    });
+    req.on("end", () => {
+      res.end(body);
+    });
+  }
+  res.end(req.url);
+});
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log("something bad happened", err);
+  }
+  console.log(`server is listening on ${port}`);
+});
