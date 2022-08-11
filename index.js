c62a1ac85afcff1fbafef82becb3f3c0a00f467a
Xenia Anichkina <xenia.anichkina@oracle.com>
2022-08-11 12:27:38 +0400

    task 5 - fix mongoose connection

--- index.js
+++ index.js
@@ -6,23 +6,29 @@ require("dotenv").config();
 const app = express();
 const PORT = 3000;
 
-app.get("/products", function (req, res) {
-  Product.find({}, function (err, products) {
-    if (err) return console.log(err);
-    res.send(products);
-  });
+mongoose.connect(process.env.DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+let db = mongoose.connection;
+db.on("error", (error) => {
+  console.error("error in MongoDb connection: " + error);
+});
+db.on("connected", () => {
+  console.log("Connected successfully");
 });
 
-mongoose.connect(
-  process.env.DATABASE_URL,
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  },
-  function (err) {
-    if (err) return console.log(err);
-    app.listen(3000, function () {
-      console.log(`server started on PORT ${PORT}`);
-    });
+//get отрабатывает даже если нет продуктов, но если есть ошибка - не отрабатывает вообще никак
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  console.log(products);
+  try {
+    res.send(products);
+  } catch (error) {
+    res.status(500).send(error);
   }
-);
+});
+
+app.listen(3000, function () {
+  console.log(`server started on PORT ${PORT}`);
+});
