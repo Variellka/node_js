5507b7e71072aafd5634c105cac1cff7b730e666
Xenia Anichkina <xenia.anichkina@oracle.com>
2022-09-07 11:23:32 +0300

    task 7 - add eslint, prettier and husky

--- db/product-model.ts
+++ db/product-model.ts
@@ -1,5 +1,5 @@
-import { prop, getModelForClass } from "@typegoose/typegoose";
-import { ObjectId } from "mongodb";
+import { prop, getModelForClass } from '@typegoose/typegoose';
+import { ObjectId } from 'mongodb';
 
 class ProductClass {
   @prop()
--- index.ts
+++ index.ts
@@ -1,29 +1,29 @@
-import express from "express";
-import mongoose from "mongoose";
-import { ProductModel } from "./db/product-model";
-require("dotenv").config();
+import express from 'express';
+import mongoose from 'mongoose';
+import { ProductModel } from './db/product-model';
+require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 
-const base_URL: string = process.env.DATABASE_URL || "";
+const base_URL: string = process.env.DATABASE_URL || '';
 mongoose.connect(base_URL);
 
 let db = mongoose.connection;
-db.on("error", (error: string) => {
-  console.error("error in MongoDb connection: " + error);
+db.on('error', (error: string) => {
+  console.error('error in MongoDb connection: ' + error);
   mongoose.disconnect();
 });
-db.on("connected", () => {
-  console.log("MongoDB connected successfully");
+db.on('connected', () => {
+  console.log('MongoDB connected successfully');
 });
-db.on("disconnected", () => {
-  console.log("MongoDB disconnected");
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected');
 });
 
-app.get("/products", async (req, res) => {
+app.get('/products', async (req, res) => {
   if (db.readyState !== 1) {
-    res.send("something went wrong...");
+    res.send('something went wrong...');
   }
   const products = await ProductModel.find({});
   res.send(products);
