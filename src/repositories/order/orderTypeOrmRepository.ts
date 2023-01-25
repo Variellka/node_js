6e53a05dea761a9f4c31fed2fb8ff3615d9b8a69
Xenia Anichkina <xenia.anichkina@oracle.com>
2023-01-25 12:41:28 +0300

    added typeorm update function

--- src/repositories/order/orderTypeOrmRepository.ts
+++ src/repositories/order/orderTypeOrmRepository.ts
@@ -13,7 +13,40 @@ import { Product } from '../../db/postgres/models/product-model';
 import { OrderListProducts } from '../../db/postgres/models/order-list-products-model';
 
 export default class OrderTypeOrmRepository implements IOrderTypeOrmRepository {
-  update: (userId: string, productId: string, quantity: number) => Promise<IOrderListProducts[] | null>;
+  public async update(userId: string, productId: string, quantity: number): Promise<IOrderListProducts[] | null> {
+    const orderRepository = AppDataSource.getRepository(OrderList);
+    const orderProductsRepository = AppDataSource.getRepository(OrderListProducts);
+
+    const order = await orderRepository
+      .createQueryBuilder('order-list')
+      .where('order-list.user._id = :userId', { userId })
+      .getOne();
+
+    const orderProduct = await orderProductsRepository
+      .createQueryBuilder('order-list-products')
+      .where('order-list-products.order_id = :id', { id: order?._id })
+      .andWhere('order-list-products.productId = :productId', { productId })
+      .getOne();
+
+    if (orderProduct) {
+      if (quantity) {
+      } else if (!quantity) {
+        await orderProductsRepository.remove(orderProduct);
+      }
+    } else if (!orderProduct) {
+      const product = await AppDataSource.getRepository(Product)
+        .createQueryBuilder('product')
+        .where('product.id = :productId', { productId })
+        .getOne();
+      await AppDataSource.getRepository(OrderListProducts).save({
+        quantity,
+        product,
+        order,
+      } as IOrderListPostgresProducts);
+    }
+
+    return await this.getByUserId(userId);
+  }
 
   public async getByUserId(id: string): Promise<IOrderListProducts[] | null> {
     const orderRepository = AppDataSource.getRepository(OrderList);
